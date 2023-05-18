ce0585beacdfc693c945cbeb12f6a85e4fce3c32
Herbert Wolverson <herberticus@gmail.com>
2023-05-18 10:45:25 -0500

    Return types

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,10 +1,20 @@
+fn double(n: i32) -> i32 {
+    n * 2
+}
+
+#[allow(clippy::needless_return)]
+fn double_or_nothing(n : i32) -> i32 {
+    if n > 0 {
+        return n * 2;
+    } else {
+        return 0;
+    }
+}
+
 fn main() {
-    let n = {
-        let mut accumulator = 0;
-        for _i in 0..10 {
-            accumulator += 1;
-        }
-        accumulator // No semicolon means "return from this scope"
-    };
-    println!("{n}");
+    let n = double(2);
+    let a = double_or_nothing(2);
+    let b = double_or_nothing(-2);
+    println!("{n} {a} {b}");
+    println!("{}", double(2));
 }
