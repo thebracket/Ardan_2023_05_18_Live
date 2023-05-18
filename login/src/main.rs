e4898be74afa298689f952e38733bef629cd49cb
Herbert Wolverson <herberticus@gmail.com>
2023-05-18 11:23:58 -0500

    Simple enum

--- login/src/main.rs
+++ login/src/main.rs
@@ -2,12 +2,36 @@ use auth::*;
 
 fn read_line() -> String {
     let mut input = String::new();
-    std::io::stdin().read_line(&mut input).expect("Failed to read line");
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
     input.trim().to_string()
 }
 
 fn main() {
-    hello_world();
-    let input = read_line();
-    println!("You typed [{input}]");
+    let mut tries = 0;
+    loop {
+        println!("Username:");
+        let username = read_line();
+        println!("Password:");
+        let password = read_line();
+
+        match login(&username, &password) {
+            LoginAction::Admin => {
+                println!("Hello admin");
+                break;
+            }
+            LoginAction::User => {
+                println!("Hello user");
+                break;
+            }
+            LoginAction::Denied => {
+                if tries > 3 {
+                    break;
+                }
+                println!("Please try again");
+                tries += 1;
+            }
+        }
+    }
 }
