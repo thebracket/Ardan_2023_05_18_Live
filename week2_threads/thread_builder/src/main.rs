110a40fa184d3a3cd9444bacf327cdd6c4417d33
Herbert Wolverson <herberticus@gmail.com>
2023-05-25 10:51:11 -0500

    Thread builder with name and stack size

---
+++ week2_threads/thread_builder/src/main.rs
@@ -0,0 +1,13 @@
+use std::thread;
+
+fn my_thread() {
+    println!("Hello from a thread named {}", thread::current().name().unwrap());
+}
+
+fn main() {
+    let handle = thread::Builder::new()
+        .name("Named Thread".to_string())
+        .stack_size(std::mem::size_of::<usize>() * 4)
+        .spawn(my_thread).unwrap();
+    handle.join().expect("oops");
+}
