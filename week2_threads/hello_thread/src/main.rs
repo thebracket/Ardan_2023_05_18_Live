8f21645ccb0eebd3182268fc10274a940277d0e8
Herbert Wolverson <herberticus@gmail.com>
2023-05-25 10:22:27 -0500

    Thread params and closures

--- week2_threads/hello_thread/src/main.rs
+++ week2_threads/hello_thread/src/main.rs
@@ -1,10 +1,14 @@
-fn hello_thread() {
-    println!("Hello from thread!");
+fn hello_thread(n: u32) {
+    println!("Hello from thread number {n}");
 }
 
 fn main() {
     println!("Hello from the main thread");
 
-    let thread_handle = std::thread::spawn(hello_thread);
-    thread_handle.join().unwrap();
+    let mut thread_handles = Vec::new();
+    for i in 0 .. 5 {
+        let handle = std::thread::spawn(move || hello_thread(i));
+        thread_handles.push(handle);
+    }
+    thread_handles.into_iter().for_each(|h| h.join().unwrap());
 }
