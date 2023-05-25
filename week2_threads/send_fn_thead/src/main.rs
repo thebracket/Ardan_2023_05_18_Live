762d938eb93d06a8745334c04b62aa05802e5f5f
Herbert Wolverson <herberticus@gmail.com>
2023-05-25 12:32:32 -0500

    Sending functions over channels

---
+++ week2_threads/send_fn_thead/src/main.rs
@@ -0,0 +1,31 @@
+use std::sync::mpsc;
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+fn hi_there() {
+    println!("Hi there!");
+}
+
+fn main() {
+    let (tx, rx) = mpsc::channel::<Job>();
+    let handle = std::thread::spawn(move || {
+        while let Ok(job) = rx.recv() {
+            job();
+        }
+    });
+
+    tx.send(Box::new(hi_there)).unwrap();
+
+    let job = || println!("Hello from the thread!");
+    tx.send(Box::new(job)).unwrap();
+
+    let job2 = || {
+        for i in 0..10 {
+            println!("i = {i}");
+        }
+    };
+    tx.send(Box::new(job2)).unwrap();
+    tx.send(Box::new(|| println!("Inline function!"))).unwrap();
+
+    handle.join().unwrap();
+}
