8d5e8d76a45652276ea202e6242a0989aa08179e
Herbert Wolverson <herberticus@gmail.com>
2023-05-25 10:28:36 -0500

    Thread returns

--- week2_threads/hello_thread/src/main.rs
+++ week2_threads/hello_thread/src/main.rs
@@ -2,13 +2,24 @@ fn hello_thread(n: u32) {
     println!("Hello from thread number {n}");
 }
 
+fn do_math(i: i32) -> i32 {
+    let mut n = i + 1;
+    for _ in 0..10 {
+        n *= 2;
+    }
+    return n;
+}
+
 fn main() {
     println!("Hello from the main thread");
 
     let mut thread_handles = Vec::new();
-    for i in 0 .. 5 {
-        let handle = std::thread::spawn(move || hello_thread(i));
+    for i in 0 .. 10 {
+        let handle = std::thread::spawn(move || do_math(i));
         thread_handles.push(handle);
     }
-    thread_handles.into_iter().for_each(|h| h.join().unwrap());
+    
+    for handle in thread_handles {
+        println!("Thread returned: {}", handle.join().unwrap());
+    }
 }
