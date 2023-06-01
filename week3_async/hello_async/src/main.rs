63ce361e4a8121080673e084882833c183998673
Herbert Wolverson <herberticus@gmail.com>
2023-06-01 10:31:31 -0500

    Tokio - manual single thread

--- week3_async/hello_async/src/main.rs
+++ week3_async/hello_async/src/main.rs
@@ -3,6 +3,16 @@ use futures::join;
 
 async fn do_work() {
     join!(say_hello(), second_fn());
+    println!("2 x 2 = {}", double(2).await);
+    not_async();
+}
+
+fn not_async() {
+    println!("I'm not async!");
+}
+
+async fn double(n: i32) -> i32 {
+    n * 2
 }
 
 async fn say_hello() {
