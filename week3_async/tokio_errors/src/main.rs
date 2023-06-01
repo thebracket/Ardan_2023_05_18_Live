79cfee877018f8543b0e9355eb1b0cc9ae89d851
Herbert Wolverson <herberticus@gmail.com>
2023-06-01 11:43:09 -0500

    Unpacking errors

--- week3_async/tokio_errors/src/main.rs
+++ week3_async/tokio_errors/src/main.rs
@@ -8,6 +8,27 @@ async fn divide(number: u32, divisor: u32) -> anyhow::Result<u32> {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    divide(5, 0).await?;
+    //divide(5, 0).await?;
+
+    let mut futures = Vec::new();
+    for n in 0..5 {
+        futures.push(divide(20, n));
+    }
+    let results = futures::future::join_all(futures).await;
+    println!("{results:#?}");
+
+    // Condense the results! ANY error makes the whole thing an error.
+    //let overall_result: anyhow::Result<Vec<u32>> = results.into_iter().collect();
+    //println!("{overall_result:?}");
+
+    // Separate the errors and the results
+    let mut errors = Vec::new();
+    let good: Vec<_> = results
+        .into_iter()
+        .filter_map(|r| r.map_err(|e| errors.push(e)).ok())
+        .collect();
+    println!("{good:?}");
+    println!("{errors:?}");
+
     Ok(())
 }
