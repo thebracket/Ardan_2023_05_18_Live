35726cbe570cdf8dfadd1cd20a67acef59dfb7e6
Herbert Wolverson <herberticus@gmail.com>
2023-06-01 10:20:07 -0500

    Minimal executor

--- week3_async/hello_async/src/main.rs
+++ week3_async/hello_async/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+use futures::executor::block_on;
+
+async fn say_hello() {
     println!("Hello, world!");
+    second_fn().await;
+}
+
+async fn second_fn() {
+    println!("Second function");
+}
+
+fn main() {
+    block_on(say_hello());
 }
