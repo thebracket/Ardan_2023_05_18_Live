1957c2b014a80498f8a3bd063182b8c3e8b9a22e
Herbert Wolverson <herberticus@gmail.com>
2023-06-01 13:09:55 -0500

    Select channels

--- week3_async/tokio_tracing/src/main.rs
+++ week3_async/tokio_tracing/src/main.rs
@@ -1,7 +1,28 @@
+use tracing_subscriber::fmt::format::FmtSpan;
+
+#[tracing::instrument]
+async fn hello_world() {
+    println!("hello, world!");
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // Applications that receive events need to subscribe
-    let subscriber = tracing_subscriber::FmtSubscriber::new();
+    let subscriber = tracing_subscriber::fmt()
+        // Use a more compact, abbreviated log format
+        .compact()
+        // Display source code file paths
+        .with_file(true)
+        // Display source code line numbers
+        .with_line_number(true)
+        // Display the thread ID an event was recorded on
+        .with_thread_ids(true)
+        // Don't display the event's target (module path)
+        .with_target(true)
+        // Include span events
+        .with_span_events(FmtSpan::ENTER | FmtSpan::CLOSE)
+        // Build the subscriber
+        .finish();
 
     // Set the subscriber as the default
     tracing::subscriber::set_global_default(subscriber)?;
@@ -10,6 +31,9 @@ async fn main() -> anyhow::Result<()> {
     tracing::info!("Starting up");
     tracing::warn!("Are you sure this is a good idea?");
     tracing::error!("This is an error!");
+
+    // Trace this:
+    hello_world().await;
 
     Ok(())
 }
