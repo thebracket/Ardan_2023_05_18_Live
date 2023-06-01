debdb292b6c9919aaadc4b4ecd823b51deb22f77
Herbert Wolverson <herberticus@gmail.com>
2023-06-01 11:52:42 -0500

    Async file i/o

--- week3_async/file_io/src/main.rs
+++ week3_async/file_io/src/main.rs
@@ -4,7 +4,9 @@ use std::path::Path;
 
 // Taken from: https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
@@ -21,17 +23,46 @@ async fn line_count(filename: String) -> anyhow::Result<usize> {
             }
         });
     }
-    println!("Read {} lines in {:.3} seconds", line_count, now.elapsed().as_secs_f32());
+    println!(
+        "Read {} lines in {:.3} seconds",
+        line_count,
+        now.elapsed().as_secs_f32()
+    );
+    Ok(line_count)
+}
+
+async fn async_line_count(filename: String) -> anyhow::Result<usize> {
+    use tokio::fs::File;
+    use tokio::io::AsyncBufReadExt;
+    use tokio::io::BufReader;
+
+    println!("Reading {filename}...");
+    let now = std::time::Instant::now();
+    let mut line_count = 0;
+
+    let file = File::open(filename).await?;
+    let reader = BufReader::new(file);
+    let mut lines = reader.lines(); // Create a stream of lines
+    while let Some(line) = lines.next_line().await? {
+        if !line.trim().is_empty() {
+            line_count += 1;
+        }
+    }
+
+    println!(
+        "Read {} lines in {:.3} seconds",
+        line_count,
+        now.elapsed().as_secs_f32()
+    );
     Ok(line_count)
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Reading ...");
     let now = std::time::Instant::now();
     let (c1, c2) = tokio::join!(
-        line_count("warandpeace.txt".to_string()),
-        line_count("warandpeace.txt".to_string())
+        async_line_count("warandpeace.txt".to_string()),
+        async_line_count("warandpeace.txt".to_string())
     );
     println!("Total lines: {}", c1? + c2?);
     println!("In {:.3} seconds", now.elapsed().as_secs_f32());
