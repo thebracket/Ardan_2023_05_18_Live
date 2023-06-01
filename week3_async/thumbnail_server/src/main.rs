c1d402150f8fc3ec87a43e88b253ffa37c5f4f62
Herbert Wolverson <herberticus@gmail.com>
2023-06-01 13:39:31 -0500

    Dependency INjection

--- week3_async/thumbnail_server/src/main.rs
+++ week3_async/thumbnail_server/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+use axum::{Extension, Router, routing::get};
+use sqlx::Row;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // Read the .env file and obtain the database URL
+    dotenv::dotenv()?;
+    let db_url = std::env::var("DATABASE_URL")?;
+
+    // Get a database connection pool
+    let pool = sqlx::SqlitePool::connect(&db_url).await?;
+
+    // Run Migrations
+    sqlx::migrate!("./migrations").run(&pool).await?;
+
+    // Build Axum with an "extension" to hold the database connection pool
+    let app = Router::new()
+        .route("/", get(test))
+        .layer(Extension(pool));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+
+    Ok(())
+}
+
+async fn test(Extension(pool): Extension<sqlx::SqlitePool>) -> String {
+    let result = sqlx::query("SELECT COUNT(id) FROM images")
+        .fetch_one(&pool)
+        .await
+        .unwrap();
+    let count = result.get::<i64, _>(0);
+    format!("{count} images in the database")
 }
