814dd14447ee716574976cc5462788aec9e9e3e0
Herbert Wolverson <herberticus@gmail.com>
2023-06-01 11:31:56 -0500

    Error handling with generic boxed results

--- week3_async/error_handling/src/main.rs
+++ week3_async/error_handling/src/main.rs
@@ -10,9 +10,30 @@ fn file_to_uppercase() -> Result<String, std::io::Error> {
     Ok(contents.to_uppercase())
 }
 
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct User { name: String, password: String }
+
+type GenericResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+fn load_users() -> GenericResult<Vec<User>> {
+    let my_file = Path::new("users.json");
+    let raw_text = std::fs::read_to_string(my_file)?;
+    let users: Vec<User> = serde_json::from_str(&raw_text)?;
+    Ok(users)
+}
+
 fn main() {
-    let _ = file_to_uppercase();
-    if let Ok(contents) = file_to_uppercase() {
-        // We have the contents!
+    let users = load_users();
+    match users {
+        Ok(users) => {
+            for user in users {
+                println!("User: {}, {}", user.name, user.password);
+            }
+        },
+        Err(err) => {
+            println!("Error: {err}");
+        }
     }
 }
