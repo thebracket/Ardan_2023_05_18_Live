c4dd62c03cdf86262ff1f2fdcbe3fec6f5d3783d
Herbert Wolverson <herberticus@gmail.com>
2023-06-01 11:24:45 -0500

    ? operator and pass-through errors

--- week3_async/error_handling/src/main.rs
+++ week3_async/error_handling/src/main.rs
@@ -1,16 +1,18 @@
 use std::path::Path;
 
-fn main() {
+fn maybe_read_a_file() -> Result<String, std::io::Error> {
     let my_file = Path::new("mytile.txt");
-    // This yields a Result type of String or an error
-    let contents = std::fs::read_to_string(my_file);
-    // Let's just handle the error by printing it out
-    match contents {
-        Ok(contents) => println!("File contents: {contents}"),
-        Err(e) => match e.kind() {
-            std::io::ErrorKind::NotFound => println!("File not found"),
-            std::io::ErrorKind::PermissionDenied => println!("Permission denied"),
-            _ => println!("ERROR: {e:#?}"),
-        },
+    std::fs::read_to_string(my_file)
+}
+
+fn file_to_uppercase() -> Result<String, std::io::Error> {
+    let contents = maybe_read_a_file()?;
+    Ok(contents.to_uppercase())
+}
+
+fn main() {
+    match file_to_uppercase() {
+        Ok(text) => println!("File contents: {text}"),
+        Err(e) => println!("An error occurred: {e:?}"),
     }
 }
