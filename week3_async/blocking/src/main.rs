f9eb2cc8c801220759c23c257d2f608868b7daff
Herbert Wolverson <herberticus@gmail.com>
2023-06-01 11:09:13 -0500

    Spawn blocking

--- week3_async/blocking/src/main.rs
+++ week3_async/blocking/src/main.rs
@@ -1,9 +1,14 @@
 use std::time::Duration;
+use tokio::task::spawn_blocking;
 
 async fn hello_delay(task: u64, time: u64) {
     println!("Task {task} has started");
     //std::thread::sleep(Duration::from_millis(time));
-    tokio::time::sleep(Duration::from_millis(time)).await;
+    //tokio::time::sleep(Duration::from_millis(time)).await;
+    let result = spawn_blocking(move || {
+        std::thread::sleep(Duration::from_millis(time));
+    }).await;
+    println!("Task {task} result {result:?}");
     println!("Task {task} is done.")
 }
 
@@ -14,4 +19,12 @@ async fn main() {
         futures.push(hello_delay(i, i * 500));
     }
     futures::future::join_all(futures).await;
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn simple() {
+        assert_eq!(2+2, 4);
+    }
 }
