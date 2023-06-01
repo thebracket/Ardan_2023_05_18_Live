b7dcf0a7e2df6dd7d3f6b248963f1ae6cefea579
Herbert Wolverson <herberticus@gmail.com>
2023-06-01 10:44:36 -0500

    Join set and join_all

--- week3_async/hello_tokio/src/main.rs
+++ week3_async/hello_tokio/src/main.rs
@@ -2,7 +2,23 @@ async fn hello() {
     println!("Hello!");
 }
 
+async fn double(n: i32) -> i32 {
+    n * 2
+}
+
 #[tokio::main]
 async fn main() {
     tokio::join!(hello(), hello());
+    let futures = vec![double(2), double(3), double(4)];
+    let result = futures::future::join_all(futures).await;
+    println!("{result:?}");
+
+    // Use Tokio JoinSet
+    let mut set = tokio::task::JoinSet::new();
+    for i in 0..10 {
+        set.spawn(double(i));
+    }
+    while let Some(res) = set.join_next().await {
+        println!("{res:?}");
+    }
 }
