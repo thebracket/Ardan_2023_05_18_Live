58c101decdbeea2f034c238ac9bbe0b8f51f53d9
Herbert Wolverson <herberticus@gmail.com>
2023-06-01 13:15:06 -0500

    SQL - Fetching data the hard way

--- week3_async/database/src/main.rs
+++ week3_async/database/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::Row;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // Read the .env file and obtain the database URL
+    dotenv::dotenv()?;
+    let db_url = std::env::var("DATABASE_URL")?;
+
+    // Get a database connection pool
+    let pool = sqlx::SqlitePool::connect(&db_url).await?;
+
+    // Fetch the messages from the database
+    let messages = sqlx::query("SELECT id, message FROM messages")
+        .map(|row: sqlx::sqlite::SqliteRow| {
+            let id: i64 = row.get(0);
+            let message: String = row.get(1);
+            (id, message)
+        })
+        .fetch_all(&pool)
+        .await?;
+
+    // Print the messages
+    for (id, message) in messages {
+        println!("{id}: {message}");
+    }
+
+    Ok(())
 }
