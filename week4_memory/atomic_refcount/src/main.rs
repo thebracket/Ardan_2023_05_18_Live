e9f60f5f6d6d3c7dae045d9a1d31cb57c0b04bfd
Herbert Wolverson <herberticus@gmail.com>
2023-06-08 11:13:18 -0500

    Atomic Ref Count

---
+++ week4_memory/atomic_refcount/src/main.rs
@@ -0,0 +1,43 @@
+use std::sync::Arc;
+
+#[derive(Debug)]
+struct Droppable(i32);
+
+impl Drop for Droppable {
+    fn drop(&mut self) {
+        println!("Dropping {:?}", self);
+    }
+}
+
+impl Droppable {
+    fn new(n: i32) -> Self {
+        Self(n)
+    }
+}
+
+fn move_me(x: Arc<Droppable>) {
+    println!("Moved {}", x.0);
+}
+
+fn main() {
+    let my_shared = Arc::new(Droppable::new(1));
+    {
+        let _x = my_shared.clone();
+        let _y = my_shared.clone();
+        let _z = my_shared.clone();
+    }
+    move_me(my_shared.clone());
+
+    let mut threads = Vec::new();
+    for _ in 0.. 10 {
+        let my_shared = my_shared.clone();
+        threads.push(std::thread::spawn(move || {
+            move_me(my_shared)
+        }));
+    }
+    for t in threads {
+        t.join().unwrap();
+    }
+
+    println!("{my_shared:?}");
+}
