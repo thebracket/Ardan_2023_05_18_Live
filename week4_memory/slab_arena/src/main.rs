5c238ec56277ab66267d9e89c0960e0e4c81d9e7
Herbert Wolverson <herberticus@gmail.com>
2023-06-08 13:23:54 -0500

    Packing

---
+++ week4_memory/slab_arena/src/main.rs
@@ -0,0 +1,14 @@
+use slab::Slab;
+
+fn main() {
+    let mut slab = Slab::with_capacity(10);
+    let hello = slab.insert("hello");
+    let world = slab.insert("world");
+
+    assert_eq!(slab[hello], "hello");
+    assert_eq!(slab[world], "world");
+
+    slab.remove(hello);
+
+    // 1 2 (3) 4 5
+}
