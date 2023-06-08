6281e0131fc50e1384eb7d93f0d8683726949c7d
Herbert Wolverson <herberticus@gmail.com>
2023-06-08 10:38:06 -0500

    RAII intro

--- week4_memory/raii/src/main.rs
+++ week4_memory/raii/src/main.rs
@@ -1,3 +1,49 @@
+#[derive(Debug)]
+struct MyStruct {
+    n: i32,
+}
+
+impl MyStruct {
+    fn new(n: i32) -> Self {
+        Self { n }
+    }
+}
+
+#[derive(Debug)]
+struct HasDrop {
+    id: i32,
+    d: MyStruct,
+}
+
+impl HasDrop {
+    fn new(id: i32) -> Self {
+        Self { id, d: MyStruct::new(id + 100) }
+    }
+}
+
+impl Drop for HasDrop {
+    fn drop(&mut self) {
+        println!("Dropping {:?}", self);
+    }
+}
+
+impl Drop for MyStruct {
+    fn drop(&mut self) {
+        println!("Dropping {:?}", self);
+    }
+}
+
+fn move_me(x: MyStruct) {
+    println!("Moved {x:?}");
+}
+
 fn main() {
-    println!("Hello, world!");
+    {
+        let ms = MyStruct::new(1);
+        move_me(ms);
+        println!("Ending scope");
+    }
+    println!("Ended scope");
+
+    let hd = HasDrop::new(12);
 }
