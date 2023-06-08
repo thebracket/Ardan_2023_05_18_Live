df3b5df973f13694647d1f364eb304419141e456
Herbert Wolverson <herberticus@gmail.com>
2023-06-08 12:15:29 -0500

    Make traits, box them and be dynamic

--- week4_memory/make_trait/src/main.rs
+++ week4_memory/make_trait/src/main.rs
@@ -2,15 +2,37 @@ trait Animal {
     fn speak(&self);
 }
 
+#[derive(Debug, Clone)]
 struct Cat;
 
 impl Animal for Cat {
     fn speak(&self) {
-        println!("Meow");
+        println!("mew");
     }
+}
+
+#[derive(Debug, Clone)]
+struct Dog;
+
+impl Animal for Dog {
+    fn speak(&self) {
+        println!("woof");
+    }
+}
+
+fn get_animal() -> impl Animal {
+    Dog
 }
 
 fn main() {
     let cat = Cat;
     cat.speak();
+    let dog = Dog;
+    dog.speak();
+
+    let fetched_animal = get_animal();
+    fetched_animal.speak();
+
+    let animals: Vec<Box<dyn Animal>> = vec![Box::new(Cat), Box::new(Dog)];
+    let (tx, rx) = std::sync::mpsc::channel::<Box<dyn Animal>>();
 }
