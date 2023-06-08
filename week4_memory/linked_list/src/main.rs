4fd2904ad34510e0693767c2554cf0b67e1a3f23
Herbert Wolverson <herberticus@gmail.com>
2023-06-08 13:07:36 -0500

    Use weak pointers

--- week4_memory/linked_list/src/main.rs
+++ week4_memory/linked_list/src/main.rs
@@ -1,9 +1,16 @@
-use std::{rc::Rc, cell::RefCell};
+use std::{cell::RefCell, rc::{Rc, Weak}};
 
 #[derive(Debug)]
 struct Node {
     value: i32,
-    next: RefCell<Option<Rc<Node>>>
+    next: RefCell<NextNode>,
+}
+
+#[derive(Debug)]
+enum NextNode {
+    None,
+    Strong(Rc<Node>),
+    Weak(Weak<Node>),
 }
 
 impl Drop for Node {
@@ -15,15 +22,14 @@ impl Drop for Node {
 fn main() {
     let tail = Rc::new(Node {
         value: 1,
-        next: RefCell::new(None),
+        next: RefCell::new(NextNode::None),
     });
     let head = Rc::new(Node {
         value: 2,
-        next: RefCell::new(Some(tail.clone())),
+        next: RefCell::new(NextNode::Strong(tail.clone())),
     });
 
-    // Let's break things
-    *tail.next.borrow_mut() = Some(head.clone());
+    *tail.next.borrow_mut() = NextNode::Weak(Rc::downgrade(&head));
 
-    //println!("head: {head:?}");
+    println!("head: {head:?}");
 }
