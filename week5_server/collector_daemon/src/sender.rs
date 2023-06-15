b00553cb89ecb29c22760b6ccc842a8e5e195c58
Herbert Wolverson <herberticus@gmail.com>
2023-06-15 13:36:38 -0500

    Mark where to add a loop for channels on reused sockets

--- week5_server/collector_daemon/src/sender.rs
+++ week5_server/collector_daemon/src/sender.rs
@@ -4,10 +4,15 @@ use std::io::Write;
 use crate::CollectorError;
 
 pub fn send_command(command: CollectorCommandV1) -> Result<(), CollectorError> {
-    let bytes = shared_data::encode_v1(command);
-    println!("Encoded {} bytes", bytes.len());
     let mut stream = std::net::TcpStream::connect(DATA_COLLECTOR_ADDRESS)
         .map_err(|_| CollectorError::UnableToConnect)?;
+
+    // Loop over receive
+    let bytes = shared_data::encode_v1(command);
+    println!("Encoded {} bytes", bytes.len());
+
     stream.write_all(&bytes).map_err(|_| CollectorError::UnableToConnect)?;
+    // End Loop
+
     Ok(())
 }
