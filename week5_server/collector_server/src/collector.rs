cef8f53354550d0de8c4e80c4ba7b0a76abd281e
Herbert Wolverson <herberticus@gmail.com>
2023-06-15 12:16:23 -0500

    SQLX

--- week5_server/collector_server/src/collector.rs
+++ week5_server/collector_server/src/collector.rs
@@ -1,29 +1,35 @@
-use std::net::SocketAddr;
-use shared_data::DATA_COLLECTOR_ADDRESS;
+use shared_data::CollectorCommandV1;
 use shared_data::decode_v1;
+use shared_data::DATA_COLLECTOR_ADDRESS;
+use sqlx::{Pool, Sqlite};
+use std::net::SocketAddr;
+use tokio::io::AsyncReadExt;
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
-use tokio::io::AsyncReadExt;
 
-pub async fn data_collector() -> anyhow::Result<()> {
+pub async fn data_collector(cnn: Pool<Sqlite>) -> anyhow::Result<()> {
     // Listen for TCP connections
     let listener = TcpListener::bind(DATA_COLLECTOR_ADDRESS).await?;
 
     // Loop forever
     loop {
         // Wait for a connection
+        let cnn = cnn.clone();
         let (socket, address) = listener.accept().await?;
-        tokio::spawn(new_connection(socket, address));
+        tokio::spawn(new_connection(socket, address, cnn));
     }
 
     Ok(())
 }
 
-async fn new_connection(mut socket: TcpStream, address: SocketAddr) {
+async fn new_connection(mut socket: TcpStream, address: SocketAddr, cnn: Pool<Sqlite>) {
     println!("New connection from {address:?}");
     let mut buf = vec![0u8; 1024];
     loop {
-        let n = socket.read(&mut buf).await.expect("Failed to read data from socket");
+        let n = socket
+            .read(&mut buf)
+            .await
+            .expect("Failed to read data from socket");
         if n == 0 {
             println!("No data received - connection closed");
             return;
@@ -31,6 +37,34 @@ async fn new_connection(mut socket: TcpStream, address: SocketAddr) {
 
         println!("Received {n} bytes");
         let received_data = decode_v1(&buf[0..n]);
-        println!("Received data: {received_data:?}");
+        //println!("Received data: {received_data:?}");
+
+        match received_data {
+            (
+                timestamp,
+                CollectorCommandV1::SubmitData {
+                    collector_id,
+                    total_memory,
+                    used_memory,
+                    average_cpu_usage,
+                },
+            ) => {
+                let collector_id = uuid::Uuid::from_u128(collector_id);
+                let collector_id = collector_id.to_string();
+
+                let result = sqlx::query("INSERT INTO timeseries (collector_id, received, total_memory, used_memory, average_cpu) VALUES ($1, $2, $3, $4, $5)")
+                    .bind(collector_id)
+                    .bind(timestamp)
+                    .bind(total_memory as i64)
+                    .bind(used_memory as i64)
+                    .bind(average_cpu_usage)
+                    .execute(&cnn)
+                    .await;
+
+                if result.is_err() {
+                    println!("Error inserting data into the database: {result:?}");
+                }
+            }
+        }
     }
 }
--- week5_server/collector_server/src/main.rs
+++ week5_server/collector_server/src/main.rs
@@ -2,7 +2,14 @@ mod collector;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let handle = tokio::spawn(collector::data_collector());
+    // Read the .env file and obtain the database URL
+    dotenv::dotenv()?;
+    let db_url = std::env::var("DATABASE_URL")?;
+
+    // Get a database connection pool
+    let pool = sqlx::SqlitePool::connect(&db_url).await?;
+
+    let handle = tokio::spawn(collector::data_collector(pool.clone()));
 
     // Wait
     handle.await??;
