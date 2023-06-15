2e343d28533eb231502b62c1641365d72929eb8c
Herbert Wolverson <herberticus@gmail.com>
2023-06-15 12:05:09 -0500

    Generate or load UUID in the collector

--- week5_server/collector_daemon/src/data_collector.rs
+++ week5_server/collector_daemon/src/data_collector.rs
@@ -2,7 +2,7 @@ use shared_data::CollectorCommandV1;
 use sysinfo::{SystemExt, CpuExt};
 use std::{time::Instant, sync::mpsc::Sender};
 
-pub fn collect_data(tx: Sender<CollectorCommandV1>) {
+pub fn collect_data(tx: Sender<CollectorCommandV1>, collector_id: u128) {
     // Init sysinfo
     let mut sys = sysinfo::System::new_all();
 
@@ -29,7 +29,7 @@ pub fn collect_data(tx: Sender<CollectorCommandV1>) {
 
         // Submit
         let send_result = tx.send(CollectorCommandV1::SubmitData { 
-            collector_id: 0, 
+            collector_id, 
             total_memory, 
             used_memory, 
             average_cpu_usage,
