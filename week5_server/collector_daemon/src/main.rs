cf63bcf4911bf41706f100b55ed90a9c1363d6a4
Herbert Wolverson <herberticus@gmail.com>
2023-06-15 11:23:37 -0500

    Data collector module with function and a channel

--- week5_server/collector_daemon/src/main.rs
+++ week5_server/collector_daemon/src/main.rs
@@ -1,3 +1,14 @@
+use shared_data::CollectorCommandV1;
+
+mod data_collector;
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = std::sync::mpsc::channel::<CollectorCommandV1>();
+
+    // Start the collector thread
+    let collector_thread = std::thread::spawn(move || {
+        data_collector::collect_data(tx);
+    });
+
+    let _ = collector_thread.join();
 }
